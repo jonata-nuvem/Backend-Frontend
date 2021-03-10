4fb537bb131fd9f9a79ab97fd03183074b2c3024
Jonata <jonatafrancisco@gmail.com>
2021-03-10 20:21:45 -0300

    projeto pós

--- src/main/java/com/example/accessingdatamysql/AccessingDataMysqlApplication.java
+++ src/main/java/com/example/accessingdatamysql/AccessingDataMysqlApplication.java
@@ -2,6 +2,9 @@ package com.example.accessingdatamysql;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class AccessingDataMysqlApplication {
@@ -9,5 +12,16 @@ public class AccessingDataMysqlApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AccessingDataMysqlApplication.class, args);
 	}
-
+@Bean
+public WebMvcConfigurer corsConfigure() {
+	return new WebMvcConfigurer() {
+		@Override
+		public void addCorsMappings(CorsRegistry registry) {
+			registry.addMapping("/**")
+			.allowedOrigins("http://localhost:4200")
+			.allowedMethods("GET", "POST", "PUT", "DELETE");
+		}
+	};
 }
+}
+
